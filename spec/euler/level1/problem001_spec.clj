ddf05bf78033849e8285fe85000d4217d3c4f34b
Alex Root-Roatch <arootroatch@gmail.com>
2024-05-22 11:53:47 -0500

    complete euler 1

--- spec/euler/level1/problem001_spec.clj
+++ spec/euler/level1/problem001_spec.clj
@@ -1,15 +1,24 @@
 (ns euler.level1.problem001-spec
   (:use
-    [speclj.core]
-    [euler.level1.problem001]))
+    [euler.level1.problem001]
+    [speclj.core]))
 
 ; http://projecteuler.net/index.php?section=problems&id=1
 
 (describe "Euler Problem #1"
 
+  (it "knows multiple of 3 and 5"
+    (should= false (multiple-of-3-or-5? 0))
+    (should= true (multiple-of-3-or-5? 6))
+    (should= true (multiple-of-3-or-5? 10)))
+
+  (it "returns the multiples of 3 or 5 from the range of a number"
+    (should= [3 5 6 9] (multiples-less-than 10))
+    (should= [3 5 6 9 10 12 15 18] (multiples-less-than 20)))
+
   (it "Solves #1"
     (should= 23 (euler-1 10))
-	(should= -1 (euler-1 1000)))
+    (should= 233168  (euler-1 1000)))
   )
 
 (run-specs)
--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -1,4 +1,14 @@
 (ns euler.level1.problem001)
 
-(defn euler-1 [n]
-  )
+
+(defn multiple-of-3-or-5? [number]
+  (if (= number 0)
+    false
+    (or (= (mod number 3) 0) (= (mod number 5) 0)))
+    )
+
+(defn multiples-less-than [number]
+  (filter multiple-of-3-or-5? (range number)))
+
+(defn euler-1 [number]
+  (reduce + (multiples-less-than number)))
