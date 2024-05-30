a871d89d2902f94b3cf6cf6d5db461b09aaec44e
Alex Root-Roatch <arootroatch@gmail.com>
2024-05-30 10:25:10 -0500

    refactor 4

--- src/euler/level1/problem004.clj
+++ src/euler/level1/problem004.clj
@@ -8,11 +8,13 @@
   (loop [factor-1 999
          factor-2 999
          products []]
-    (if (= 100 factor-1 factor-2)
-      (filter palindrome? products)
-      (if (= factor-1 100)
+    (cond
+      (= 100 factor-1 factor-2)
+        (filter palindrome? products)
+      (= factor-1 100)
         (recur (dec factor-2) (dec factor-2) (conj products (* factor-1 factor-2)))
-        (recur (dec factor-1) factor-2 (conj products (* factor-1 factor-2)))))))
+      :else
+        (recur (dec factor-1) factor-2 (conj products (* factor-1 factor-2))))))
 
 (defn euler-4 []
   (first (sort > (get-palindrome-products))))
